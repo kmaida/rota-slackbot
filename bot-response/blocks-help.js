ec7635c0955ea124e45d04fc793c539bd2b9371b
Kim Maida <yi.mihi@gmail.com>
2020-06-30 09:38:00 -0400

    Add link to App Home in help block

--- bot-response/blocks-help.js
+++ bot-response/blocks-help.js
@@ -5,7 +5,19 @@ const commandsArr = require('./blocks-commands');
     BLOCKS: HELP
 ------------------*/
 const helpBlocks = () => {
-  return introArr.concat(commandsArr);
+  const appHome = [
+    {
+      "type": "section",
+      "text": {
+        "type": "mrkdwn",
+        "text": `:house: *Visit your <slack://app?team=${process.env.SLACK_TEAM_ID}&id=${process.env.SLACK_APP_ID}&tab=home|App Home>* to find out which rotations you're currently on _active duty_ or _on staff_ for.`
+      }
+    },
+    {
+      "type": "divider"
+    }
+  ];
+  return introArr.concat(appHome).concat(commandsArr);
 };
 
 module.exports = helpBlocks;
